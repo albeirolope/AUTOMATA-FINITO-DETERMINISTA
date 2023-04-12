16c8ca2ee7c4cda4c2687e7c2e17e4fd7d60bc13
Luis Mario Brugés <albeirolopezoviedo@gmail.com>
2023-04-12 16:09:27 -0500

    se agrego el proyecto

---
+++ index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+app=Flask(__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='password'
+app.config['MYSQL_DB']='afd'
+mysql=MySQL(app)
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+ 
+
+
+if __name__=="__main__":
+    app.run(port=3000,debug=True)
